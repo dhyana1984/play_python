8effc1cbc21de375695275c7e47b071605e379d5
Chris <chris.yee@qq.com>
2018-07-16 22:03:58 +0800

    实现同时min或者max函数
    实现同时min或者max函数

---
+++ minmax.py
@@ -0,0 +1,9 @@
+def minmax(test,*args):
+    res = args[0]
+    for arg in args[1:]:
+        if test(arg, res):
+            res = arg
+    return res
+
+def lessthan(x,y): return x<y
+def grterthan(x,y): return x>y
